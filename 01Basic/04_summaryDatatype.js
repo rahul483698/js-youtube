8cd387cd7cc9fe91484c8202fc683fa6d414c5d1
rahul483698 <137701369+rahul483698@users.noreply.github.com>
2024-08-23 11:02:58 +0000

    DataType Summary

---
+++ 01Basic/04_summaryDatatype.js
@@ -0,0 +1,24 @@
+//primitive
+//7 type==string,boolean,number,null,undefined,Symbled,BigInt
+
+const score=100
+const ScoreValue=100.3
+const isLoggedIn=false
+const outSideTemp=null
+let userEmail;
+
+const id=Symbol("123")
+const AnotherId=Symbol("123")
+console.log(id==AnotherId)
+
+
+//Refrence(non primitive)
+//Array,function,Object
+
+const Hero=["Saktiman","naagraj"]
+
+let Obj={
+    name:"rahul",
+    age:22,
+}
+
