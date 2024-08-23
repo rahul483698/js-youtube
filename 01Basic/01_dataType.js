df5d599677dd14a27525e5eedd2c8e34252e1a8f
rahul483698 <137701369+rahul483698@users.noreply.github.com>
2024-08-23 10:08:48 +0000

    data type

---
+++ 01Basic/01_dataType.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all js code as newer version
+// alert(3+3)//we are using node js not browser
+
+let name="rahul kumar"
+let age=21
+let isLoggedIn=false
+
+// number=>2 to power 53
+// BigInt
+// string=>""
+// boolean=>true,false
+//null=>standalone value(khali hai)
+// undefined=>agar value assigned n hua ho kisi variable me
+//symbol=>unique
+
+//object
+
+console.log(typeof "rahul")
+console.log(typeof undefined)//undefined
+console.log(typeof null)//abject
+
