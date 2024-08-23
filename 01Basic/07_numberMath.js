deb298ba79affddeca6976d1313b2fc3c6f118c7
rahul483698 <137701369+rahul483698@users.noreply.github.com>
2024-08-23 13:54:02 +0000

    number Math

---
+++ 01Basic/07_numberMath.js
@@ -0,0 +1,41 @@
+const score=400
+//console.log(score)
+
+const balance=new Number(400)
+//console.log(balance)
+
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(2));
+
+const otherNumber=123.899//try to 23.899
+//console.log(otherNumber.toPrecision(3))
+
+const Hundred=1000000
+//console.log(Hundred.toLocaleString("en-In"));
+
+
+
+
+//+++++++++++++++++Math+++++++++++++++
+
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.6))
+// console.log(Math.ceil(4.7))
+// console.log(Math.floor(7.9))
+// console.log(Math.min(3,5,8,1))
+// console.log(Math.max(2,6,3,7))
+
+console.log(Math.random());//0 and 1 ke between koi bhi value dega
+console.log(Math.random()*10);
+ console.log((Math.random()*10)+1);
+console.log(Math.floor((Math.random()*10)+1));
+
+const min=10
+const max=20
+
+console.log(Math.floor((Math.random()*(max-min+1))+min));
+
+
+
+
