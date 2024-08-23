af940d1acb22366fcef10af0ff06334f281b1571
rahul483698 <137701369+rahul483698@users.noreply.github.com>
2024-08-23 10:07:43 +0000

    conversionOperation

---
+++ 01Basic/02_ConversionOperation.js
@@ -0,0 +1,23 @@
+let score=33
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+//"33"=>33
+//"33abc"=>NaN
+//"Null"=>0
+//"true"=>1
+//"undefined"=>NaN
+/**************opration********** */
+let str1="hello";
+let str2="rahul";
+let str3=str1+str2;
+console.log(str3);
+
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+3)
+console.log(1+2+"3")
